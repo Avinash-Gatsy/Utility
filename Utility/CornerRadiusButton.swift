3087f1843fd5b763331090588011052cc42fc505
Avinash <avinashg12291992@outlook.com>
2017-02-05 00:24:27 +0530

    Added Gradient to Login & Sign Up buttons

--- Utility/CornerRadiusButton.swift
+++ Utility/CornerRadiusButton.swift
@@ -11,18 +11,30 @@ import UIKit
 @IBDesignable
 class CornerRadiusButton: UIButton {
     
-    @IBInspectable var cornerRadius : CGFloat = 30.0 {
-        didSet{
-            setupUI()
-        }
-    }
+    @IBInspectable var startColor: UIColor = UIColor.red
+    @IBInspectable var endColor: UIColor = UIColor.green
+    @IBInspectable var cornerRadius : CGFloat = 30.0
+    override func draw(_ rect: CGRect) {
+
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: UIRectCorner.allCorners,
+                                cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+        path.addClip()
+        
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [startColor.cgColor, endColor.cgColor]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        let gradient = CGGradient(colorsSpace: colorSpace,
+                              colors: colors as CFArray,
+                              locations: colorLocations)
     
-    override func prepareForInterfaceBuilder() {
-        setupUI()
-    }
-    
-    func setupUI(){
-         layer.cornerRadius = cornerRadius;
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x:path.bounds.width, y:path.bounds.height)
+        context?.drawLinearGradient(gradient!,
+                                start: startPoint,
+                                end: endPoint,
+                                options: CGGradientDrawingOptions(rawValue: 0))
     }
     
 }
